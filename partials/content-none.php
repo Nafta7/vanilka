4e24fda6b1ef656f5d270ea434358ff6fc78896a
Nefla <neflasan@gmail.com>
2017-06-01 17:49:28 -0300

    add partials

---
+++ partials/content-none.php
@@ -0,0 +1,7 @@
+<div>
+  <h1>No results</h1>
+  <p>
+    Nothing matched with the search terms. Please try again with different
+    keywords.
+  </p>
+</div>
---
+++ partials/content.php
@@ -0,0 +1,10 @@
+<article class="">
+  <h1>
+    <a href="<?php the_permalink(); ?>">
+      <?php the_title(); ?>
+    </a>
+  </h1>
+  <div class="">
+    <?php the_content(); ?>
+  </div>
+</article>
