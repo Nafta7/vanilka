df7aa8eea2d26a318439fd713673f6670acef875
Nefla <neflasan@gmail.com>
2017-06-01 16:24:28 -0300

    remove hardcoded link

--- header.php
+++ header.php
@@ -3,7 +3,6 @@
   <head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width">
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,800" rel="stylesheet">
     <title><?php bloginfo('name'); ?></title>
     <?php wp_head(); ?>
 
