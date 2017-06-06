1133a6af5288f3af73bc0edd541a7df8cb70a8d1
Nefla <neflasan@gmail.com>
2017-06-06 18:57:11 -0300

    add standard page template

---
+++ templates/standard-page.php
@@ -0,0 +1,16 @@
+<?php /* Template Name: Standard Page Template */ ?>
+<?php
+  get_header();
+
+  if (have_posts()) {
+    while (have_posts()) {
+      the_post();
+      get_template_part('partials/page/content', 'page');
+    }
+  } else {
+    get_template_part( 'partials/post/content', 'none' );
+  }
+
+  get_footer();
+
+?>
