7be6126ae4170a090c163c5dc4c653171e1b5cde
Nefla <neflasan@gmail.com>
2017-06-07 17:14:39 -0300

    add 404 page

---
+++ 404.php
@@ -0,0 +1,11 @@
+<?php
+get_header(); ?>
+
+<div>
+  <h1>Page not found</h1>
+  <p>Unfortunately, this page does not exist. Maybe try a search?</p>
+  <?php get_search_form(); ?>
+</div>
+
+<?php
+get_footer(); ?>
